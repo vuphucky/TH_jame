26e6d88e5666f61861d4f33de07481b91496a4bf
vuphucky <phucky1235@gmail.com>
2024-06-11 09:18:56 +0700

    lan dau

---
+++ TH4_variable.js
@@ -0,0 +1,6 @@
+inputwidth = prompt("Nhập chiều rộng");
+inputheight = prompt("nhập chiều dài");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write('The area is: ' + area +"                 " + "Chieu dai:"+ inputheight +"           " + "chiều rộng: " + inputwidth);
